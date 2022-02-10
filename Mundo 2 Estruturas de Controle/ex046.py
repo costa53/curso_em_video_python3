a15e8c987673c44598022c09a71d1a84ce91f609
Costa Lima <paulo@hotmail.sk>
2022-02-10 15:19:46 -0300

    Contagem regressiva

---
+++ Mundo 2 Estruturas de Controle/ex046.py
@@ -0,0 +1,11 @@
+# DESAFIO 046
+# Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
+# indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+import emoji
+from time import sleep
+
+for c in range(10, -1, -1):
+    sleep(1)
+    print(c)
+print(emoji.emojize(':fireworks: BOOM!! FOGOS ESTOURANDO!! :fireworks:', use_aliases=True))
