1ecf33e030d187ae7cf721f4fb0cca14063c53a8
Costa Lima <paulo@hotmail.sk>
2022-02-10 08:41:48 -0300

    Somando dois números

---
+++ Mundo 1 Fundamentos/ex003.py
@@ -0,0 +1,7 @@
+# DESAFIO 003
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = (n1+n2)
+print(f'A some entre {n1} e {n2} é igual a {soma}')
