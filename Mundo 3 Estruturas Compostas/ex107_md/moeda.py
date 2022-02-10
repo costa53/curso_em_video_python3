5a81e53e7973d4307d474ae2fa8ead6809afa0bd
Costa Lima <paulo@hotmail.sk>
2022-02-10 19:24:18 -0300

    Exercitando módulos em Python

---
+++ Mundo 3 Estruturas Compostas/ex107_md/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(n, a=0):
+    n += (n * a / 100)
+    return n
+
+
+def diminuir(n, a=0):
+    n -= (n * a / 100)
+    return n
+
+
+def dobro(n):
+    r = n * 2
+    return r
+
+
+def metade(n):
+    r = n / 2
+    return r
