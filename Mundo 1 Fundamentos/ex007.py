4ddd7bd50e13e044352c07a45dae618b073d2ac5
Costa Lima <paulo@hotmail.sk>
2022-02-10 08:45:54 -0300

    Média Aritmética

---
+++ Mundo 1 Fundamentos/ex007.py
@@ -0,0 +1,7 @@
+# DESAFIO 007
+# Desenvolva um programa que mostre as duas notas de um aluno, calcule e mostre a sua média.
+
+nt1 = float(input('Primeira nota do aluno: '))
+nt2 = float(input('Segunda nota do aluno: '))
+m = (nt1 + nt2) / 2
+print(f'A média entre {nt1} e {nt2} é igual a {m:.1f}')
