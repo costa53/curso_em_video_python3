c677a7d20a0baa4a85ebef6e20a09a12d3797e88
Costa Lima <paulo@hotmail.sk>
2022-02-10 08:52:33 -0300

    Primeira e última ocorrência de uma string

---
+++ Mundo 1 Fundamentos/ex026.py
@@ -0,0 +1,10 @@
+# DESAFIO 026
+# Faça um programa que leia uma frase pelo teclado e mostre:
+# Quantas vezes aparece a letra 'A';
+# Em que posição ela aparece pela primeira vez;
+# Em que posição ela aparece pela última vez.
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+print(f'A letra "A" aparece {frase.count("A")} vezes na frase')
+print(f'A primeira letra "A" apareceu na posição {frase.find("A")+1}')
+print(f'A última letra "A" apareceu na posição {frase.rfind("A")+1}')
