c98facf56ca601ca0f07ecf87d2bb993aabeadc6
Costa Lima <paulo@hotmail.sk>
2022-02-10 08:42:23 -0300

    Antecessor e Sucessor

---
+++ Mundo 1 Fundamentos/ex005.py
@@ -0,0 +1,5 @@
+# DESAFIO 005
+# Faça um programa que leia um número inteiro e mostre na tela e o seu sucessor e o seu antecessor.
+
+n = int(input('Digite um número: '))
+print(f'Analisando o valor {n}, seu antecessor é o {n-1} e o sucessor é o {n+1}')
