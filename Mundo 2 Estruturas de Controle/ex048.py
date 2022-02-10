95356758117769fbc75c64b21792506f8dfa0109
Costa Lima <paulo@hotmail.sk>
2022-02-10 15:20:15 -0300

    Soma ímpares múltiplos de três

---
+++ Mundo 2 Estruturas de Controle/ex048.py
@@ -0,0 +1,11 @@
+# DESAFIO 048
+# Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de 3 e que
+# se encontram no intervalo de 1 até 500.
+
+soma = 0
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        soma += c
+        cont += 1
+print(f'A soma de todos os {cont} valores solicitados é {soma}')
